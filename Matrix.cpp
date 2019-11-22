913025c22eca55d64bef72fe04f54048371e1a5a
joaolsa <joaoluizdoss@gmail.com>
2019-11-22 10:09:23 -0300

    Classe matrix

--- Matrix.cpp
+++ Matrix.cpp
@@ -7,7 +7,20 @@ class Matrix {
    private:
       unsigned m_rowSize;
       unsigned m_colSize;
-      vector<vector<int> > m_matrix;
+      float** matrix;
+      void construct(unsigned rowSize,
+      unsigned colSize) {
+         m_rowSize = rowSize;
+         m_colSize = colSize;
+         matrix = new float*[m_rowSize];
+         for (int i = 0; i < m_rowSize; ++i)
+            matrix[i] = new float[m_colSize];
+      }
    public:
       Matrix determinant();
+      void deleteMatrix(void) {
+         for (int i = 0; i < m_rowSize; ++i)
+            delete [] matrix[i];
+         delete [] matrix;
+      }
 };
