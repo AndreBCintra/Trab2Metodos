742ffdd3dde5242f46c60e360ebd64f0ee1fb6c6
joaolsa <joaoluizdoss@gmail.com>
2019-11-25 13:52:29 -0300

    Calcular determinante e metodos de Gauss

--- Matrix.cpp
+++ Matrix.cpp
@@ -16,8 +16,35 @@ class Matrix {
          for (int i = 0; i < m_rowSize; ++i)
             matrix[i] = new float[m_colSize];
       }
+      void escolhe_pivo(int k, int *pivo, int *r) {
+      }
    public:
-      Matrix determinant();
+      Matrix escalonarGauss() {
+         for (int k = 1; k <= m_rowSize - 1; ++k ) {
+            int *pivo, *r;
+            escolhe_pivo(k, &pivo, &r);
+            if (pivo == 0) {
+               throw "A matriz e singular";
+            }
+            if (r != k) {
+               permuta(pivo, k, r);
+            }
+            for (int i = k + 1; i <= m_rowSize; ++i) {
+               int m = matrix[i][k]/matrix[k][k];
+               matrix[i][k] = m;
+               for (int j = k+1; j <= m_rowSize; ++j) {
+                  matrix[i][j] = matrix[i][j] - m*matrix[k][j];
+               }
+            }
+         }
+      }
+      Matrix determinante() { // Recebe uma matriz triangular
+         int det = 1;
+         for (int i = 0; i < m_rowSize) {
+            det = det * matrix[i][i];
+         }
+         return det;
+      }
       void deleteMatrix(void) {
          for (int i = 0; i < m_rowSize; ++i)
             delete [] matrix[i];
