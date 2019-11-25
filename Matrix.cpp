2b3632130ca34250c2a8c4148cc7cb6a641d0514
AndreBCintra <49659483+AndreBCintra@users.noreply.github.com>
2019-11-24 21:07:26 -0300

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -17,10 +17,41 @@ class Matrix {
             matrix[i] = new float[m_colSize];
       }
    public:
+      Matrix(int rowSize, int colSize) {
+         construct(m_rowSize, colSize);
+      }
       Matrix determinant();
       void deleteMatrix(void) {
          for (int i = 0; i < m_rowSize; ++i)
             delete [] matrix[i];
          delete [] matrix;
+      }
+      int getSize(){
+         return m_rowSize;
+      }
+      void addMatrix(int row, int col, float value){
+         matrix[row][col] = value;
+      }
+      float getMatrix(int row, int col){
+         return matrix[row][col];
+      }
+      void opRow(int row, int baseRow, float value){
+         for (int i = 0; i < m_rowSize; ++i){
+            matrix[row][i] = matrix[row][i] - (matrix[baseRow][i]*value);
+        }
+      }
+      void pivot(int col){
+         int row_max = col;
+         for (int i = col + 1; i < m_colSize; ++i){
+            if (abs(matrix[i][col]) > abs(matrix[row_max][col])){
+               row_max = i;
+            }
+         }
+         float aux;
+         for (int i = 0; i < m_rowSize; ++i){
+            aux = matrix[row_max][i];
+            matrix[row_max][i] = matrix[col][i];
+            matrix[col][i] = aux;
+         }
       }
 };
