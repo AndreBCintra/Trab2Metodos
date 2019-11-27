43a063549dcad9c7652334b99f178602164eed09
leozintuf <58182700+leozintuf@users.noreply.github.com>
2019-11-27 08:04:23 -0300

    Update LUNormal.cpp

--- LUNormal.cpp
+++ LUNormal.cpp
@@ -3,6 +3,26 @@
 class LUNormal {
     public:
         void solveLinearSystem(Matrix system) {
-            
+            int* subst_suscessivas_mod(int m_rowSize, float** matrix, float* b){
+                float x[m_rowSize]=0, soma;
+                for(int i=0; i<=m_rowSize-1; i++){
+                    soma = 0;
+                    for(int j=0; j<=i-1; j++){
+                        soma = soma + matrix[i][j]*x[j];
+                    }
+                    x[i] = b[i] - soma;
+                }
+            }
+
+            int* subst_retroativa(int m_rowSize, float** matrix, float* y){
+                float x[m_rowSize] = y[m_rowSize]/matrix[m_rowSize][m_rowSize], soma; 
+                for(i=m_rowSize-2; i<=0; i--){
+                    soma = 0;
+                    for(j=i+1; j=m_rowSize-1){
+                        soma = soma + matrix[i][j]*x[j];
+                    }
+                x[i] = (y[i] - soma)/matrix[i][i];
+                }
+            }
         }
 };
